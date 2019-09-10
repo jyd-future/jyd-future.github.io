d0f2ad69500ee3f4ee49e1194ef8bfdbb1bffc0e
jyd-future <danid.gonzalez@alumnos.duoc.cl>
2019-09-09 21:15:53 -0300

    añadi paginas nuevas y contenido nuevo

--- js/subir-al-cielo.js
+++ js/subir-al-cielo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $('.up').hide();
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 500) {
             $('.up').slideDown(300);
         } else {
             $('.up').slideUp(300);
